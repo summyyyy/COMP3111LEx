05b0e37ef20c7e9544259b3f4a41f997763e47d1
summyyyy <sylamau@connect.ust.hk>
2023-09-13 20:34:37 +0800

    second commit

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -4,6 +4,8 @@ public class mainApp1 {
     public static void main(String[] args){
         int b = 2;
         int n = 11;
+        int a = 33;
+        int aa = 44;
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = "+ myLibrary.factorial(n));
         System.out.println("Program ..Ended ...");
